3683ac83197f86ba7b3ac2a600eceaf6bc281276
unknown <abdelrhman_m@outlook.com>
2019-09-07 22:14:23 +0200

    Finished;

--- src/main/views.py
+++ src/main/views.py
@@ -3,37 +3,47 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 from bs4 import Tag
+import time
 
 # Create your views here.
 def result(request):
+    start_time = time.time()
     f = open("./countries/countries.txt", "r")
     countries  = []
-    salaryList = []
-    packList   = []
+    avgVSpack  = []
+    avgSalary  = []
     for x in f:
-      country_name = x.rstrip()
-      url = "https://www.numbeo.com/cost-of-living/country_result.jsp?country="+str(country_name)+"&displayCurrency=USD"
-      try:
-        page = urllib.request.urlopen(url)
-      except:
-        print("An error occured.")
-
+      country = x.rstrip()
+      url = "https://www.numbeo.com/cost-of-living/country_result.jsp?country={}&displayCurrency=USD".format(country)
+      page = urllib.request.urlopen(url)
       soup = BeautifulSoup(page, 'html.parser')
-      if 'have that country in the database' in soup.text or 'No data for this country' in soup.text:
-          print('Not Found Country:::')
-      else:
-          print(country_name)
-          avgSalary = soup.find(lambda t: t.text.strip()=='Average Monthly Net Salary (After Tax)').parent.select('td')
-          packPrice = soup.find(lambda t: t.text.strip()=='Cigarettes 20 Pack (Marlboro)').parent.select('td')
+      if 'Average Monthly Net Salary' in soup.text :
+          get_avgSalary = soup.find(lambda t: t.text.strip()=='Average Monthly Net Salary (After Tax)').parent.select('td')
+          get_packPrice = soup.find(lambda t: t.text.strip()=='Cigarettes 20 Pack (Marlboro)').parent.select('td')
           country = soup.find('span',attrs={'class':'purple_light'}).text
           countries.append(country)
-          for average, salary, ciggrates ,price in zip(avgSalary, avgSalary[1:],packPrice,packPrice[1:2]):
-            sal = salary.text
-            prc = price.text
-            salaryList.append(sal)
-            packList.append(prc)
-
-    myData = zip(countries,salaryList,packList)
+          for salary,price in zip(get_avgSalary[1:],get_packPrice[1:2]):
+            salary = salary.text
+            packPrice = price.text
+            if soup.find('option',{'selected':'selected','value':'USD'}):
+                filter_salry = salary.replace('$','').strip().replace(',','')
+                filter_price = packPrice.replace('$','').strip()
+                if filter_price == '?' or filter_salry == '?':
+                    filter_price = 0
+                    filter_salry = 0
+                else:
+                    ConvertSalToFloating = float(filter_salry)
+                    ConverPriceToFloating = float(filter_price)
+                    Avrg_Pack_Price_In_Country = round(ConvertSalToFloating,2)/round(ConverPriceToFloating,2)
+                    avrgWithStr = str(Avrg_Pack_Price_In_Country)
+                    avgVSpack.append(avrgWithStr[:6])
+                    avgSalary.append(salary)
+                    print(country)
+            else:
+                pass
+      else:
+          pass
+    myData = zip(countries,avgVSpack,avgSalary)
     context = {
                 'myData':myData,
                 }
